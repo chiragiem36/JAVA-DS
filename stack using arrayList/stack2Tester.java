d1b6d21b7d7c9f35f332e95fa22733ab1da17115
Chirag Jain <chiragiem36@gmail.com>
2017-05-03 22:06:38 +0530

    initialized

---
+++ stack using arrayList/stack2Tester.java
@@ -0,0 +1,6 @@
+public class stack2Tester{
+	public static void main(String[] args){
+		Stack2 stk = new Stack2();
+		stk.enterSyntax();
+	}
+}
---
+++ stack/stackTester.java
@@ -0,0 +1,9 @@
+public class stackTester{
+
+	public static void main(String[] args){
+		Stack stk = new Stack();
+		stk.enterSyntax();
+		
+	}
+
+}
